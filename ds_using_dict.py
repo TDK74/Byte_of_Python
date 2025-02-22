77a2584f73bc8724b7f4845911c41fcc6720cd12
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 16:15:19 +0200

    Added a new example file from the book.

---
+++ ds_using_dict.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+ab = {
+    'Swaroop': 'swaroop@swaroopch.com',
+    'Larry': 'larry@wall.org',
+    'Matsumoto': 'matz@ruby-lang.org',
+    'Spammer': 'spammer@hotmail.com'
+}
+
+print("Swaroop's address is", ab['Swaroop'])
+
+# Deleting a key-value pair
+del ab['Spammer']
+
+print('\nThere are {} contacts in the address-book\n'.format(len(ab)))
+
+for name, address in ab.items():
+    print('Contact {} at {}'.format(name, address))
+
+# Adding a key-value pair
+ab['Guido'] = 'guido@python.org'
+
+if 'Guido' in ab:
+    print("\nGuido's address is", ab['Guido'])
+
+# help(dict) <- for the list of methods of the dict class
