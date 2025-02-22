2d31122193929b210d666231fc9ad48a9be0559d
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 15:57:13 +0200

    Added a new example file.

---
+++ 1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''This is a multi-line string. This is the first line.
+This is the second line.
+"What's your name?," I asked.
+He said "Bond, James Bond."
+'''
+
+'This is the first line\nThis is the second line'
+
+"This is the first sentence. \
+This is the second sentence."
+
+"This is the first sentence. This is the second sentence."
+
+r"Newlines are indicated by \n"
+
