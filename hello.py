dbfa230afe33c7b130f4f3619299a5cbafc33cbb
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 19:29:02 +0200

    Added a new example file from the book.

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+print("hello world")
+
+print('hello world')    # Note that print is a function
+
+# Note that print is a function
+print('hello world')
+
