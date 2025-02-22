36b4bfa514aae4c9c3a874224c5f6f7b4b7fdaef
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 21:43:10 +0200

    Added a new example file from the book.

---
+++ var.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Filename : var.py
+i = 5
+print(i)
+i = i + 1
+print(i)
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
+
+i = 5
+print(i)
+i = 5;
+print(i);
+i = 5; print(i);
+i = 5; print(i)
+
+s = 'This is a string. \
+This continues the string.'
+print(s)
+
+i = \
+5
+
+i = 5
+# Error below! Notice a single space at the start of the line
+ print('Value is', i)
+print('I repeat, the value is', i)
