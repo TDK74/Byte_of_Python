e2544d12d909b2e5cd0e8f39b779e5be97ba047a
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 21:42:44 +0200

    Added a new example file from the book.

---
+++ str_format.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+age = 20
+name = 'Swaroop'
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
+
+name + ' is ' + str(age) + ' years old'
+
+age = 20
+name = 'Swaroop'
+print('{} was {} years old when he wrote this book'.format(name, age))
+print('Why is {} playing with that python?'.format(name))
+
+# decimal (.) precision of 3 for float '0.333'
+print('{0:.3f}'.format(1.0/3))
+# fill with underscores (_) with the text centered
+# (^) to 11 width '___hello___'
+print('{0:_^11}'.format('hello'))
+# keyword-based 'Swaroop wrote A Byte of Python'
+print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
+
+print('a', end='')
+print('b', end='')
+
+print('a', end=' ')
+print('b', end=' ')
+print('c')
+
