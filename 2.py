bcdc049ead6f410ce404978716cb922bb2ae2bd8
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 15:57:39 +0200

    Added a new example file.

---
+++ 2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+2 + 3
+3 * 5
+'a' + 'b'
+50 - 24
+2 * 3
+'la' * 3
+3 ** 4
+13 / 3
+13 // 3
+-13 // 3
+13 % 3
+2 << 2
+11 >> 1
+5 & 3
+5 | 3
+5 ^ 3
+~5
+5 < 3
+3 < 5
+3 < 5 < 7
+5 > 3
+x = 3; y = 6; x <= y
+x = 4; y = 3; x >= 3
+x = 2; y = 2; x == y
+x = 'str'; y = 'stR'; x == y
+x = 'str'; y = 'str'; x == y
+x = 2; y = 3; x != y
+x = 2; y = 3; x != y
+x = False; y = True; x and y
+x = True; y = False; x or y
+a = 2
+a = a * 3
+a = 2
+a *= 3
+
+
