17ed02c5ab09cf175b5e36fd2a982454ce3a67b5
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 19:28:13 +0200

    Added a new example file from the book.

---
+++ function_return.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def maximum(x, y):
+    if x > y:
+        return x
+    elif x == y:
+        return 'The numbers are equal'
+    else:
+        return y
+
+
+print(maximum(2, 3))
+
+
+def some_function():
+    pass
